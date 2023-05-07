da7e487803d38f46dc83cb8d23b3efe8b201b725
ruili1 <45102740+ruili1@users.noreply.github.com>
2023-05-06 23:57:10 -0500

    Update tournament.py
    fixed wrongly committed code

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -1,11 +1,19 @@
-class Team(object):
+class Tournament(object):
 
-    def __init__(self, teamId, teamName, teamDesc):
-        self.teamId = teamId
-        self.teamName = teamName
-        self.teamDesc = teamDesc
+    def __init__(self, tournamentId, tournamentName, addressLine1, addressLine2, city, state, zip):
+        self.tournamentId = tournamentId
+        self.tournamentName = tournamentName
+        self.addressLine1 = addressLine1
+        self.addressLine2 = addressLine2
+        self.city = city
+        self.state = state
+        self.zip = zip
 
     def __str__(self):
-        return ("{teamId: " + str(self.teamId) +
-                ", teamName: " + self.teamName +
-                ", teamDesc: " + self.teamDesc + "}")
+        return ("{tournamentId: " + str(self.tournamentId) +
+                ", tournamentName: " + self.tournamentName +
+                ", addressLine1: " + self.addressLine1 + 
+                ", addressLine2: " + self.addressLine2 + 
+                ", city: " + self.city + 
+                ", state: " + self.state +
+                ", zip: " + self.zip + "}")
